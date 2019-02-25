d1e699c8046e35bc817e22989a6981c31c5dbcc9
Mahyar <mahyarjananeh289@gmail.com>
2019-02-25 17:06:44 +0330

    add gitignore file + complete createThread functionality

--- ChatAutomationTestApp/MyViewController.swift
+++ ChatAutomationTestApp/MyViewController.swift
@@ -258,6 +258,22 @@ extension MyViewController {
     
     
     
+    
+    /*
+     This method will create a thread with some specific users
+     when calling this method,
+     if the caller of this function filled the "invitees" and "title" -> then we create a thread with the ginven data
+     else:
+     we asume that we have to test every single situation to create thead including:
+     addContact + create CHANNEL Thread with contact CellPhoneNumber (using TO_BE_USER_CELLPHONE_NUMBER)
+     addContact + create CHANNEL_GROUP Thread with contact CellPhoneNumber (using TO_BE_USER_CELLPHONE_NUMBER)
+     addContact + create OWNER_GROUP Thread with contact CellPhoneNumber (using TO_BE_USER_CELLPHONE_NUMBER)
+     addContact + create PUBLIC_GROUP Thread with contact CellPhoneNumber (using TO_BE_USER_CELLPHONE_NUMBER)
+     addContact + create NORMAL Thread with contact CellPhoneNumber (using TO_BE_USER_CELLPHONE_NUMBER)
+     addContact + create NORMAL Thread with contact id (using TO_BE_USER_CONTACT_ID)
+     addContact + create NORMAL Thread with contact linkeUser id (using TO_BE_USER_ID)
+     addContact + create NORMAL Thread with contact linkeUser usename (using TO_BE_USER_USERNAME)
+     */
     func createThread(description: String?, image: String?, invitees: [Invitee]?, metadata: String?, title: String?, type: String?, requestUniqueId: String?,
                       uniqueId:       @escaping (String) -> (),
                       serverResponse: @escaping (CreateThreadModel) -> ()) {
@@ -273,24 +289,161 @@ extension MyViewController {
             
         }
         
-        // if the input parameter didn't filled by the user, first add a contact, then create a thread with this contact
-        if (description == nil) && (invitees == nil) && (title == nil) && (type == nil) {
+        
+        
+        
+        /*
+         first create a contact, in the response ContactModel there is parameter nameed "id"
+         in the invitee model to pass to createThread, pass this "id" and type "TO_BE_USER_CONTACT_ID"
+         */
+        func createNormalThreadWithContactId(cellphoneNumber: String?, email: String?, firstName: String?, lastName: String?) {
             
-            addContact(cellphoneNumber: nil, email: nil, firstName: nil, lastName: nil, uniqueId: { _ in }) { (contactModel) in
+            addContact(cellphoneNumber: cellphoneNumber, email: email, firstName: firstName, lastName: lastName, uniqueId: { _ in }) { (contactModel) in
                 if let myContact = contactModel.contacts.first {
-                    if let contactPhoneNumber = myContact.cellphoneNumber {
+                    if let contactId = myContact.id {
                         
                         let fakeParams = self.generateFakeCreateThread()
-                        let myInvitee = Invitee(id: contactPhoneNumber, idType: "\(InviteeVOidTypes.TO_BE_USER_CELLPHONE_NUMBER.rawValue)")
+                        let myInvitee = Invitee(id: "\(contactId)", idType: "\(InviteeVOidTypes.TO_BE_USER_CONTACT_ID.rawValue)")
                         sendRequest(theDescription: fakeParams.description, theImage: image, theInvitees: [myInvitee], theMetadata: metadata, theTitle: fakeParams.title, theType: fakeParams.type, theRequestUniqueId: nil)
                         
                     } else {
-                        // handle error that didn't get contact CellphoneNumber in the contact model
+                        // handle error that didn't get contact id in the contact model
                     }
                 } else {
                     // handle error that didn't add Contact Model
                 }
             }
+            
+        }
+        
+        /*
+         first create a contact, then use it linkeUser model,
+         insid linkeUser, there is parameter nameed "coreUserId"
+         in the invitee model to pass to createThread, pass this "coreUserId" and type "TO_BE_USER_ID"
+         */
+        func createNormalThreadWithId(cellphoneNumber: String?, email: String?, firstName: String?, lastName: String?) {
+            
+            addContact(cellphoneNumber: cellphoneNumber, email: email, firstName: firstName, lastName: lastName, uniqueId: { _ in }) { (contactModel) in
+                if let myContact = contactModel.contacts.first {
+                    if myContact.hasUser {
+                        
+                        if let linkeUser = myContact.linkedUser {
+                            if let linkUserId = linkeUser.coreUserId {
+                                let fakeParams = self.generateFakeCreateThread()
+                                let myInvitee = Invitee(id: "\(linkUserId)", idType: "\(InviteeVOidTypes.TO_BE_USER_ID.rawValue)")
+                                sendRequest(theDescription: fakeParams.description, theImage: image, theInvitees: [myInvitee], theMetadata: metadata, theTitle: fakeParams.title, theType: fakeParams.type, theRequestUniqueId: nil)
+                            } else { // handle error that didn't get linkUserId in the linkedUser model of Contact
+                            }
+                            
+                        } else { // handle error that didn't get linkUser model of Contact
+                        }
+                        
+                    } else { // this contact don't have linkeUser
+                    }
+                    
+                } else { // handle error that didn't add Contact Model
+                }
+            }
+            
+        }
+        
+        /*
+         this one needs user ssoId that has not implemented yet (how to get user ssoId)
+         */
+        func createNormalThreadWithSsoId() {
+            
+        }
+        
+        /*
+         first create a contact, then use it linkeUser model,
+         insid linkeUser, there is parameter nameed "username"
+         in the invitee model to pass to createThread, pass this "username" and type "TO_BE_USER_USERNAME"
+         */
+        func createNormalThreadWithUsername(cellphoneNumber: String?, email: String?, firstName: String?, lastName: String?) {
+            
+            addContact(cellphoneNumber: cellphoneNumber, email: email, firstName: firstName, lastName: lastName, uniqueId: { _ in }) { (contactModel) in
+                if let myContact = contactModel.contacts.first {
+                    if myContact.hasUser {
+                        
+                        if let linkeUser = myContact.linkedUser {
+                            if let linkedUsername = linkeUser.username {
+                                let fakeParams = self.generateFakeCreateThread()
+                                let myInvitee = Invitee(id: linkedUsername, idType: "\(InviteeVOidTypes.TO_BE_USER_USERNAME.rawValue)")
+                                sendRequest(theDescription: fakeParams.description, theImage: image, theInvitees: [myInvitee], theMetadata: metadata, theTitle: fakeParams.title, theType: fakeParams.type, theRequestUniqueId: nil)
+                            } else { // handle error that didn't get linkUserId in the linkedUser model of Contact
+                            }
+                            
+                        } else { // handle error that didn't get linkUser model of Contact
+                        }
+                        
+                    } else { // this contact don't have linkeUser
+                    }
+                    
+                } else { // handle error that didn't add Contact Model
+                }
+            }
+            
+        }
+        
+        /*
+         first create a contact, in the response ContactModel there is parameter nameed "cellphoneNumber"
+         in the invitee model to pass to createThread, pass this "cellphoneNumber" and type "TO_BE_USER_CELLPHONE_NUMBER"
+         */
+        func createNormalThreadWithCellPhoneNumber(cellphoneNumber: String?, email: String?, firstName: String?, lastName: String?) {
+            createContactWithPhoneNumberThenCreateThread(cellphoneNumber: cellphoneNumber, email: email, firstName: firstName, lastName: lastName, threadType: ThreadTypes.NORMAL.rawValue)
+        }
+        
+        
+        func createChannelThread(cellphoneNumber: String?, email: String?, firstName: String?, lastName: String?) {
+            createContactWithPhoneNumberThenCreateThread(cellphoneNumber: cellphoneNumber, email: email, firstName: firstName, lastName: lastName, threadType: ThreadTypes.CHANNEL.rawValue)
+        }
+        
+        func createChannelGroupThread(cellphoneNumber: String?, email: String?, firstName: String?, lastName: String?) {
+            createContactWithPhoneNumberThenCreateThread(cellphoneNumber: cellphoneNumber, email: email, firstName: firstName, lastName: lastName, threadType: ThreadTypes.CHANNEL_GROUP.rawValue)
+        }
+        
+        func createOwnerGroup(cellphoneNumber: String?, email: String?, firstName: String?, lastName: String?) {
+            createContactWithPhoneNumberThenCreateThread(cellphoneNumber: cellphoneNumber, email: email, firstName: firstName, lastName: lastName, threadType: ThreadTypes.OWNER_GROUP.rawValue)
+        }
+        
+        func createPublicGroup(cellphoneNumber: String?, email: String?, firstName: String?, lastName: String?) {
+            createContactWithPhoneNumberThenCreateThread(cellphoneNumber: cellphoneNumber, email: email, firstName: firstName, lastName: lastName, threadType: ThreadTypes.PUBLIC_GROUP.rawValue)
+        }
+        
+        func createContactWithPhoneNumberThenCreateThread(cellphoneNumber: String?, email: String?, firstName: String?, lastName: String?, threadType: String) {
+            addContact(cellphoneNumber: cellphoneNumber, email: email, firstName: firstName, lastName: lastName, uniqueId: { _ in }) { (contactModel) in
+                if let myContact = contactModel.contacts.first {
+                    if let contactId = myContact.cellphoneNumber {
+                        
+                        let fakeParams = self.generateFakeCreateThread()
+                        let myInvitee = Invitee(id: "\(contactId)", idType: "\(InviteeVOidTypes.TO_BE_USER_CELLPHONE_NUMBER.rawValue)")
+                        sendRequest(theDescription: fakeParams.description, theImage: image, theInvitees: [myInvitee], theMetadata: metadata, theTitle: fakeParams.title, theType: threadType, theRequestUniqueId: nil)
+                        
+                    } else {
+                        // handle error that didn't get contact id in the contact model
+                    }
+                } else {
+                    // handle error that didn't add Contact Model
+                }
+            }
+        }
+        
+        
+        
+        
+        
+        
+        // if the input parameter didn't filled by the user, first add a contact, then create a thread with this contact
+        if (invitees == nil) && (title == nil) {
+            
+            createNormalThreadWithContactId(cellphoneNumber: "09387181694", email: nil, firstName: "Pooria", lastName: "Pahlevani")
+            createNormalThreadWithId(cellphoneNumber: "09387181694", email: nil, firstName: "Pooria", lastName: "Pahlevani")
+            createNormalThreadWithUsername(cellphoneNumber: "09387181694", email: nil, firstName: "Pooria", lastName: "Pahlevani")
+            createNormalThreadWithCellPhoneNumber(cellphoneNumber: "09387181694", email: nil, firstName: "Pooria", lastName: "Pahlevani")
+            createChannelThread(cellphoneNumber: "09387181694", email: nil, firstName: "Pooria", lastName: "Pahlevani")
+            createChannelGroupThread(cellphoneNumber: "09387181694", email: nil, firstName: "Pooria", lastName: "Pahlevani")
+            createOwnerGroup(cellphoneNumber: "09387181694", email: nil, firstName: "Pooria", lastName: "Pahlevani")
+            createPublicGroup(cellphoneNumber: "09387181694", email: nil, firstName: "Pooria", lastName: "Pahlevani")
             
         }
         // some or all of the parameters are filled by the caller, so send request with this params
@@ -299,7 +452,7 @@ extension MyViewController {
             if (title != nil) && (invitees != nil) {
                 sendRequest(theDescription: description, theImage: image, theInvitees: invitees!, theMetadata: metadata, theTitle: title!, theType: type, theRequestUniqueId: requestUniqueId)
             } else {
-                // handle that you should fill all these 4 items: description , title , invitees , type
+                // handle that you should fill all these 2 items: title , invitees
             }
             
         }
